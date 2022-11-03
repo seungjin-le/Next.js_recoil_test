2c0ca4b505c71e903bbdc159125a2609b32890a6
Seungjin Lee <dltmdwls154@gmail.com>
2022-11-03 17:06:36 +0900

    LayHeader.jsx 에서 LayoutHeader.jsx로 파일 이름 변경

--- components/layout/LayHeader.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const LayHeader = () => {
-  return <div></div>;
-};
-
-export default LayHeader;
---
+++ components/layout/LayoutHeader.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LayoutHeader = () => {
+  return <div></div>;
+};
+
+export default LayoutHeader;
